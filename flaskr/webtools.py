02169bc6d274bd4154bd587512085786b9f415c6
ywang-wnlo <ywang_wnlo@qq.com>
2025-11-30 00:02:03 +0800

    新增 iina-cli 命令行支持

--- flaskr/webtools.py
+++ flaskr/webtools.py
@@ -17,14 +17,32 @@ def decode_real_url(url):
             curr = prefix + curr.split(prefix)[-1] 
     return curr
 
+def gen_iina_cmdline(url, ext_headers):
+    """根据 URL 和扩展头生成 iina-cli 命令行"""
+    iina_cmdline = 'iina-cli'
+    lines = ext_headers.strip().split("\n")
+    for line in lines:
+        k, v = line.split(":", 1)
+        line = f'{k.strip()}: {v.strip()}'
+        iina_cmdline += f' --mpv-http-header-fields="{line}"'
+    iina_cmdline += f' "{url}"'
+    return iina_cmdline
+
 @bp.route("/quick-iina", methods=("GET", "POST"))
 def quick_iina():
     """无需登录即可访问的 quick-iina 页面，根据实际 method 处理请求"""
     real_url = None
+    iina_cmdline = None
+
     if request.method == "POST":
         input_url = request.form.get("input_url")
         if input_url.startswith("http://") or input_url.startswith("https://"):
             real_url = decode_real_url(input_url)
+            ext_headers = request.form.get("ext_headers", None)
+            if ext_headers:
+                iina_cmdline = gen_iina_cmdline(real_url, ext_headers)
+                real_url = None
         else:
             real_url = "不是有效的 URL"
-    return render_template("webtools/quick-iina.html", real_url=real_url)
+
+    return render_template("webtools/quick-iina.html", real_url=real_url, iina_cmdline=iina_cmdline)
